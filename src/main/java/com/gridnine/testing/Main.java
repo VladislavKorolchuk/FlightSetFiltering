f0b14139a1c83aad3583598737e85b1794283f14
93294 <9329496@mail.ru>
2023-07-20 00:07:16 +0300

    Execution of a test task

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -1,7 +1,25 @@
 package com.gridnine.testing;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Flight> flightList = FlightBuilder.createFlights();
+
+        FlightFilter flightFilter = new FlightFilter();
+
+        System.out.println("List of all flights:");
+        flightList.forEach(System.out::println);
+
+        System.out.printf("%nDeparture to the current point in time%n");
+        flightFilter.DepartureToTheCurrentPointInTime(flightList).forEach(System.out::println);
+
+        System.out.printf("%nThere are segments with an arrival date earlier than the departure date%n");
+        flightFilter.thereAreSegmentsWithAnArrivalDateEarlierThanTheDepartureDate(flightList).forEach(System.out::println);
+
+        System.out.printf("%nTotal time spent on the ground exceeds two hours%n");
+        flightFilter.totalTimeSpentOnTheGroundExceedsTwoHours(flightList).forEach(System.out::println);
     }
+
 }
